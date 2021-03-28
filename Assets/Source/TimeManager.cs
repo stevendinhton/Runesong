6281470ad17cdaf3b4d31983d3e695b57e682e3f
Steven Dinhton Nguyen <stevendinhton@gmail.com>
2021-03-28 07:43:55 -0400

    ingame time manager

---
+++ Assets/Source/TimeManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimeManager : MonoBehaviour { 
+
+    public static ulong DateInTicks { private set; get; }
+    public static TimeManager instance;
+
+    private int secondsPerTick = 1;
+    private float lastUpdateTime = 0; 
+
+    public void Awake() {
+        instance = this;
+    }
+
+    public void Update() {
+        float currentTime = Time.time;
+
+        if (currentTime - lastUpdateTime > secondsPerTick) {
+            DateInTicks += (ulong) ((currentTime - lastUpdateTime) / secondsPerTick);
+            lastUpdateTime = currentTime;
+        }
+    }
+}
