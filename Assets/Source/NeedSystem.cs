dbe0d4babfaa064e7b9ee0e25defc21047f34339
Steven Dinhton Nguyen <stevendinhton@gmail.com>
2021-03-30 16:38:07 -0400

    implement basic need system

--- Assets/Source/NeedSystem.cs
+++ Assets/Source/NeedSystem.cs
@@ -3,13 +3,22 @@ using UnityEngine;
 using Unity.Entities;
 
 public class NeedSystem : ComponentSystem {
+
+    private ulong lastUpdateTick = 0;
+
     protected override void OnUpdate() {
+        int ticksPassed = (int)(TimeManager.DateInTicks - lastUpdateTick);
+
+        if (ticksPassed == 0) return;
+
+        lastUpdateTick = TimeManager.DateInTicks;
+
         Entities.ForEach((Entity entity, ref PhysiologicalNeeds needs) => {
             NeedFood needFood = EntityManager.GetComponentData<NeedFood>(entity);
             NeedRest needRest = EntityManager.GetComponentData<NeedRest>(entity);
 
-            needFood.foodLevel--;
-            needRest.restLevel--;
+            needFood.foodLevel -= ticksPassed;
+            needRest.restLevel -= ticksPassed;
 
             EntityManager.SetComponentData(entity, needFood);
             EntityManager.SetComponentData(entity, needRest);
